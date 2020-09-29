f1c6618eebc51865362d11118503bd2a5c56a4e6
Omar-Labib <61431902+Omar-Labib@users.noreply.github.com>
2020-09-29 16:00:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//dice one
+var randomNumber1;
+randomNumber1 = Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+
+//dice tow
+
+var randomNumber2;
+randomNumber2 = Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML ="🏆 Player 1 Wins!";
+
+}
+else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML =" Player 2 Wins! 🏆";
+
+}
+else{
+  document.querySelector("h1").innerHTML ="🏆 Draw! 🏆";
+
+}
